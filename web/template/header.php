049a372eb9cb593d196d10c691f8e115b5c75d20
Mark Weiman <mark.weiman@markzz.com>
2016-04-02 00:27:01 -0400

    added some things to header.php
    Signed-off-by: Mark Weiman <mark.weiman@markzz.com>

--- web/template/header.php
+++ web/template/header.php
@@ -1,1 +1,14 @@
-<?php /* TODO: Add header of page here */
+<?php $LANG = 'en'; ?>
+<?= '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?= htmlspecialchars($LANG, ENT_QUOTES) ?>" lang="<?= htmlspecialchars($LANG, ENT_QUOTES) ?>">
+    <head>
+        <title>Jackson College Pathways <?php if ($title != "") { print " - " . htmlspecialchars($title); } ?></title>
+        <link rel="stylesheet" type="text/css" href="/css/pathways.css" />
+        <link rel="icon" href="images/favicon.ico" />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="description" content="JC Pathways Database" />
+    </head>
+
+    <body>
+        

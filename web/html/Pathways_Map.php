0134b32d4c8bd16cb35ca82ef34ad6021798f6f9
kldaly5430 <kldaly5430@gmail.com>
2016-04-21 15:58:17 -0400

    Added maps page html, php, and css

---
+++ web/html/Pathways_Map.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Maps</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link rel='stylesheet' id='jc-css'  href='css/jc-css.css' type='text/css' media='all' />
+  <link rel='stylesheet' id='searcharea'  href='css/search.css' type='text/css' media='all' />
+  <!---<link rel='stylesheet' id='jc-print-css'  href='css/header.css' type='text/css' media='print' />--->
+  <link rel='stylesheet' id='jc-font-open-sans-css'  href='css/fonts.css' type='text/css' media='all' />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  
+  <header>
+
+<div id="borderTop">
+    <div id="banner">
+        <img id="logo" src="css/Jc_Logo.png">
+    </div>
+</div>
+
+<nav>
+<ul id="nav">
+    <li><button type="button">Sign Out</button></li>
+</ul>
+</nav>
+
+</head>
+<body>
+
+<div class="container" Style="Padding-top: 54px;">
+  <h3 id="header">Please choose your program of study</h3> 
+  <div class="panel-group" Style="padding-top: 20px">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">			
+			<ul id="path">
+				<li id="program"><a Style="pointer-events: none; cursor: default; vertical-align: middle">Accounting</li>
+				<li id="type"><a href=#Associates>Associates</li>
+				<li id="type"><a href=#Account>Certificate</li>
+			</ul>
+		</h4>
+      </div>
+    </div>
+  </div>
+  
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <ul id="path">
+				<li id="program"><a Style="pointer-events: none; cursor: default;">Business Administration</li>
+                                <li id="type"><a href=#Bachelor's>Bachelor's</li>
+				<li id="type"><a href=#Associates>Associates</li>
+				<li id="type"><a href=#support>Certificate</li>
+			</ul>
+        </h4>
+      </div>
+    </div>
+  </div>
+  
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <ul id="path">
+				<li id="program"><a Style="pointer-events: none; cursor: default;">Computer Networking</li>
+				<li id="type"><a href=#Associates>Associates</li>
+				<li id="type"><a href=#support>Certificate</li>
+                                <li id="type"><a href=#Concentration>Concentration</li>
+			</ul>
+        </h4>
+      </div>
+    </div>
+  </div>
+  
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+			<ul id="path">
+				<li id="program"><a Style="pointer-events: none; cursor: default;">Computer Programming Specialist</li>
+				<li id="type"><a href=#Associates>Associates</li>
+				<li id="type"><a href=#Prgoamming>Certificate</li>
+			</ul>
+        </h4>
+      </div>
+    </div>
+  </div>
+  
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <ul id="path">
+				<li id="program"><a Style="pointer-events: none; cursor: default;">Computer Support Specialist</li>
+				<li id="type"><a href=#Associates>Associates</li>
+				<li id="type"><a href=#support>Certificate</li>
+			</ul>
+        </h4>
+      </div>
+    </div>
+  </div>
+  
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <ul id="path">
+				<li id="program"><a Style="pointer-events: none; cursor: default;">Culinary Arts</li>
+				<li id="type"><a href=#Associates>Associates</li>
+				<li id="type"><a href=#Certificate>Certificate</li>
+				<li id="type"><a href=#Concentration>Concentration</li>
+				<li id="type"><a href=#skillSet>Skill Set</li>
+			</ul>
+        </h4>
+      </div>
+    </div>
+  </div>
+  
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <ul id="path">
+				<li id="program"><a Style="pointer-events: none; cursor: default;">Cyber Security</li>
+				<li id="type"><a href=#Associates>Associates</li>
+			</ul>
+        </h4>
+      </div>
+    </div>
+  </div>
+  
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <ul id="path">
+				<li id="program"><a Style="pointer-events: none; cursor: default;">Digital Photography</li>
+				<li id="type"><a href=#Certificate>Certificate</li>
+			</ul>
+        </h4>
+      </div>
+    </div>
+  </div>
+</div>
+
+    
+</body>
+</html>

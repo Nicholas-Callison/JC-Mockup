e7b86dd4df37ae5ff438ef9bf297080b88c49d8d
Nicholas-Callison <CallisoNicholaB@my.jccmi.edu>
2016-04-26 19:24:21 -0400

    Added footer

--- web/html/Pathways_Semester.php
+++ web/html/Pathways_Semester.php
@@ -1,14 +1,16 @@
 <?php
 
-echo <<<_END
+?>
+
 <html>
 <head>
   <meta charset="utf-8">	
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel='stylesheet' id='bootstrap' href='css/bootstrap/bootstrap.css' type='text/css'>
-  <link rel='stylesheet' id='bootstrap' href='css/bootstrap/custombs.css' type='text/css'>
+  <link rel='stylesheet' id='bootstrapover' href='css/bootstrap/custombs.css' type='text/css'>
   <link rel='stylesheet' id='jc-css'  href='css/jc-css.css' type='text/css' media='all' />
-  <link rel='stylesheet' id='jc-print-css'  href='css/header.css' type='text/css' media='print' />
+  <link rel='stylesheet' id='jc-font-font-awesome-css'  href='//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css?ver=4.4.2' type='text/css' media='all' />
+  
   <link rel='stylesheet' id='jc-font-open-sans-css'  href='css/fonts.css' type='text/css' media='all' />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
@@ -24,8 +26,9 @@ echo <<<_END
 </div>
 <nav>
 
-    <li><button type="button">Sign Out</button></li>
-
+<div align="right">
+	<button type="button">Sign Out</button>
+</div>
 
 </nav>
 </head>
@@ -42,7 +45,9 @@ echo <<<_END
 				<td><a href="Pathways_Winter.php"><button class="btn btn-primary btn-lg btn-block">Fall</button></a></td>
 			</tr>
 		</table>
+
+	
+	
 </body>
 </html>
-_END;
-?>
+

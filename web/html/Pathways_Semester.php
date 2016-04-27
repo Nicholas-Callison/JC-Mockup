d5dcd59a5bce89053e5a0acde89ca0e7734c0054
Nicholas-Callison <CallisoNicholaB@my.jccmi.edu>
2016-04-26 20:06:35 -0400

    Updated headers and footers

--- web/html/Pathways_Semester.php
+++ web/html/Pathways_Semester.php
@@ -1,53 +1,22 @@
 <?php
-
+    set_include_path(get_include_path() . PATH_SEPARATOR . '../lib');
+	
+	include("pathways.inc.php");
+	html_header();
+	
+	html_footer();
 ?>
 
-<html>
-<head>
-  <meta charset="utf-8">	
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel='stylesheet' id='bootstrap' href='css/bootstrap/bootstrap.css' type='text/css'>
-  <link rel='stylesheet' id='bootstrapover' href='css/bootstrap/custombs.css' type='text/css'>
-  <link rel='stylesheet' id='jc-css'  href='css/jc-css.css' type='text/css' media='all' />
-  <link rel='stylesheet' id='jc-font-font-awesome-css'  href='//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css?ver=4.4.2' type='text/css' media='all' />
-  
-  <link rel='stylesheet' id='jc-font-open-sans-css'  href='css/fonts.css' type='text/css' media='all' />
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  
-  <header>
 
-<div id="borderTop">
-    <div id="banner">
-		<a href="http://www.jccmi.edu">
-			<img id="logo" src="css/Jc_Logo.png">
-		</a>
-    </div>
-</div>
-<nav>
-
-<div align="right">
-	<button type="button">Sign Out</button>
-</div>
-
-</nav>
-</head>
-
-<body>
-		<table align="center" style="width:100%;padding-top:500px;background-color:transparent;border-style:none;">
-			<tr>
-				<td><a href="Pathways_Fall.php"><button class="btn btn-primary btn-lg btn-block">Winter</button></a></td>
-			</tr>
-			<tr style="background-color:transparent;">
-				<td><a href="Pathways_Spring.php"><button class="btn btn-primary btn-lg btn-block">Spring</button></a></td>
-			</tr>
-			<tr>
-				<td><a href="Pathways_Winter.php"><button class="btn btn-primary btn-lg btn-block">Fall</button></a></td>
-			</tr>
-		</table>
-
-	
-	
-</body>
-</html>
+<table align="center" style="width:100%;padding-top:500px;background-color:transparent;border-style:none;">
+	<tr>
+		<td><a href="Pathways_Fall.php"><button class="btn btn-primary btn-lg btn-block">Winter</button></a></td>
+	</tr>
+	<tr style="background-color:transparent;">
+		<td><a href="Pathways_Spring.php"><button class="btn btn-primary btn-lg btn-block">Spring</button></a></td>
+	</tr>
+	<tr>
+		<td><a href="Pathways_Winter.php"><button class="btn btn-primary btn-lg btn-block">Fall</button></a></td>
+	</tr>
+</table>
 

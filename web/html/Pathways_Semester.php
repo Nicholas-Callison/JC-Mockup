fba0a03447feb79d3bd5672fb425ad9dc4b00c31
Mark Weiman <mark.weiman@markzz.com>
2016-04-27 22:42:28 -0400

    removed files that seemed to server little to no purpose
    Signed-off-by: Mark Weiman <mark.weiman@markzz.com>

--- web/html/Pathways_Semester.php
+++ web/html/Pathways_Semester.php
@@ -9,13 +9,13 @@
 
 <table align="center" style="width:100%;padding-top:500px;background-color:transparent;border-style:none;">
 	<tr>
-		<td><a href="Pathways_Fall.php"><button class="btn btn-primary btn-lg btn-block">Winter</button></a></td>
+		<td><a href="#"><button class="btn btn-primary btn-lg btn-block">Winter</button></a></td>
 	</tr>
 	<tr style="background-color:transparent;">
-		<td><a href="Pathways_Spring.php"><button class="btn btn-primary btn-lg btn-block">Spring</button></a></td>
+		<td><a href="#"><button class="btn btn-primary btn-lg btn-block">Spring</button></a></td>
 	</tr>
 	<tr>
-		<td><a href="Pathways_Winter.php"><button class="btn btn-primary btn-lg btn-block">Fall</button></a></td>
+		<td><a href="#"><button class="btn btn-primary btn-lg btn-block">Fall</button></a></td>
 	</tr>
 </table>
 

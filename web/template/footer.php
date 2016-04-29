3de94f5f272d192c5598817b6c45ce28b3222d35
Mark Weiman <mark.weiman@markzz.com>
2016-04-29 17:10:50 -0400

    fixed one more merge error from 8d1701f
    Signed-off-by: Mark Weiman <mark.weiman@markzz.com>

--- web/template/footer.php
+++ web/template/footer.php
@@ -69,10 +69,6 @@
 				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-<<<<<<< HEAD
-
-=======
->>>>>>> Kyle
 		ga('create', 'UA-9877942-1', 'auto');
 		ga('send', 'pageview');
 	</script>
